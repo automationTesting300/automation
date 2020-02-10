bad57b6ae353bf477bd4a5424cca44e1e81dbe68
automationTesting300 <60861542+automationTesting300@users.noreply.github.com>
2020-02-10 01:55:01 +0000

    Add files via upload

---
+++ Automation/UnitTest1.cs
@@ -0,0 +1,18 @@
+using AutomationTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Automation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {IWebDriver driver = new FirefoxDriver();
+            HomePage homePage = new HomePage(driver,"hey");
+            homePage.TestMethod1("hi");
+        }
+    }
+}
